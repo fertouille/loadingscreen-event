5295d59315f82a940ec5c0ab9b86d7e263678e0e
Fertouille <71925174+fertouille@users.noreply.github.com>
2024-09-28 12:19:26 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -10,38 +10,42 @@ Config.title = "Orion - CloneWars";
 /**
  * Enable map text in the top left corner of the screen?
  */
-Config.enableMap = false;
+Config.enableMap = true;
 
 /**
  * Enable steamId text in the top right corner of the screen?
  */
-Config.enableSteamID = false;
+Config.enableSteamID = true;
 
 /**
  * Enable announcements?
  */
-Config.enableAnnouncements = false;
+Config.enableAnnouncements = true;
 
 /**
  * What messages do you want to show up?
  * only works if enableAnnouncements = true
  */
 Config.announceMessages = [
-  "Bienvenue à bord de l'Orion, prêts pour le combat ?",
-  "Préparez-vous à rejoindre la bataille interstellaire !",
-  "Explorez des mondes et vivez des aventures épiques !",
-  "Consultez nos règles sur le forum pour une expérience optimale !",
-  "Besoin d'assistance ? Contactez un admin en jeu !",
-  "Rejoignez notre Discord pour des annonces et des évents !",
-  "Que la Force guide vos actions, soldat !",
-  "En avant vers la victoire pour la République !"
+  "Bienvenue sur notre serveur !",
+  "Profitez de votre expérience de jeu !",
+  "N'oubliez pas de lire nos règles sur le forum !",
+  "Rejoignez notre Discord pour rester informé !",
+  "Merci d'être parmi nous !",
+  "Amusez-vous bien !",
+  "Rappelez-vous : respectez les autres joueurs !",
+  "Besoin d'aide ? Contactez un admin en jeu !",
+  "Vous êtes perdus sur quelque chose ? Rejoignez notre discord.",
+  "Faite appel au staff en faisant un ticket en jeu, au besoin !",
+  "N'oubliez pas, ceci est un SeriousRP !",
+  "Faites attention à vos tirs...."
 ];
 
 /**
  * How many miliseconds for each announcement?
  * only works if enableAnnouncements = true
  */
-Config.announcementLength = 2700;
+Config.announcementLength = 3500; /* des fois que j'ai cassé le truc comme ça */ 
 
 /**
  * Image Filename
